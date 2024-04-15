05f5a8b9b5300ced139a60573d4e27826863f340
Nirina Prosper Herimaminavalona <nherimam@c1r1s5.42antananarivo.mg>
2024-04-15 12:49:24 +0300

    solve_less_than_4_element

--- PUSH_SWAP/algo_next.c
+++ PUSH_SWAP/algo_next.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   algo_next.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nherimam <nherimam@student.42antanana      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/15 09:33:04 by nherimam          #+#    #+#             */
+/*   Updated: 2024/04/15 09:33:07 by nherimam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
 int	first_value(t_list *stack)
@@ -44,4 +56,16 @@ int	max_value(t_list *stack)
 		stack = stack->next;
 	}
 	return (value);
+}
+
+int	median_value(t_list *stack)
+{
+	int	i;
+	int	index;
+
+	i = 0;
+	index = (ft_lstsize (stack) / 2);
+	while (i < index)
+		stack = stack->next;
+	return (stack->content);
 }
--- PUSH_SWAP/c_algo_next.c
+++ PUSH_SWAP/c_algo_next.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   c_algo_next.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nherimam <nherimam@student.42antanana      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/15 09:33:35 by nherimam          #+#    #+#             */
+/*   Updated: 2024/04/15 09:33:38 by nherimam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
 void	rotate_all(t_list **stack_a, t_list **stack_b)
@@ -14,20 +26,55 @@ void	reverse_rotate_all(t_list **stack_a, t_list **stack_b)
 	ft_printf ("rrr\n");
 }
 
+void	ft_reverse_sort_stack_b(t_list **stack_b)
+{
+//	int	cibles;
+
+	if (is_reverse_sort (stack_b[0]))
+		return ;
+}
+
+int	find_cibles(int first_v, t_list *stack_a)
+{
+	int	tmp;
+	int	cibles;
+	int	result;
+
+	if (first_v <= min_value (stack_a))
+		return (min_value (stack_a));
+	if (first_v >= max_value (stack_a))
+		return (min_value (stack_a));
+	cibles = stack_a->content;
+	while (stack_a->next != NULL)
+	{
+		tmp = cibles - first_v;
+		result = (stack_a->next)->content - first_v;
+		if (tmp < 0 || (tmp > result))
+			cibles = (stack_a->next)->content;
+		stack_a = stack_a->next;
+	}
+	return (cibles);
+}
+
 void	ft_solve_cas(t_list **stack_a, t_list **stack_b)
 {
-	if (ft_lstsize (stack_a[0]) == 3)
-	{
-		ft_solve_three (stack_a);
-		return ;
-	}
-	else if (ft_lstsize (stack_a[0]) > 3)
+	int	cibles;
+	int	first_b;
+
+	while (ft_lstsize (stack_a[0]) > 3)
 	{
 		push_b (stack_a, stack_b);
-		ft_solve_cas (stack_a, stack_b);
-		push_a (stack_b, stack_a);
+		ft_reverse_sort_stack_b (stack_b);
 	}
-	while (!(is_sort (stack_a[0])))
-		rotate_a (stack_a);
-	return ;
+	ft_solve_three (stack_a);
+	while (ft_lstsize (stack_b[0]) > 0)
+	{
+		first_b = first_value (stack_b[0]);
+		cibles = find_cibles (first_b, stack_a[0]);
+		while (cibles != first_value (stack_a[0]))
+			rotate_a (stack_a);
+		push_a (stack_b, stack_a);
+		while (!is_sort (stack_a[0]))
+			reverse_rotate_a (stack_a);
+	}
 }
